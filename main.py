6137f5e6163ae5abb552f009e0d9f80bc43db1eb
YuriyPopov312 <bas.bas.2019@list.ru>
2022-07-11 19:49:37 +0300

    Добавлена регистрация и авторизация (flask-login )

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_login import UserMixin, login_manager, login_user, login_required, logout_user, LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+app.secret_key = 'qwertyuiop[56784nfu94fk48gh6gj8g5j8'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shoes_shop.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+login_manager = LoginManager()
+login_manager.init_app(app)
 db = SQLAlchemy(app)
+
+
 
 class Shoes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,6 +23,18 @@ class Shoes(db.Model):
     def __repr__(self):
         return self.title
 
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(128), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
 @app.route('/')
 def index():
     shoes = Shoes.query.order_by(Shoes.price).all()
@@ -23,10 +42,13 @@ def index():
 
 
 @app.route('/about')
+@login_required
 def about():
     return render_template('about.html')
 
+
 @app.route('/create', methods=['POST', 'GET'])
+@login_required
 def create():
     if request.method == "POST":
         title = request.form['title']
@@ -43,6 +65,64 @@ def create():
             return "Ошибка, проверьте правильность ввода данных"
     else:
         return render_template('create.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login_page():
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if login and password:
+        user = User.query.filter_by(login=login).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            try:
+                next_page = request.args.get('next')
+                return redirect(next_page)
+            except:
+                return render_template('index.html')
+        else:
+            flash("Введен не правильный логин или пароль")
+    else:
+        flash("Заполните поля логин или пароль")
+    return render_template('login.html')
+
+
+@app.route('/logout', methods=['POST', 'GET'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login_page'))
+
+
+@app.route('/registry', methods=['POST', 'GET'])
+def registry():
+    login = request.form.get('login')
+    password = request.form.get('password')
+    password2 = request.form.get('password2')
+
+    if request.method == 'POST':
+        if not (login or password or password2):
+            flash("Заполните поля регитсрации")
+        elif password != password2:
+            flash("Пароли не совпадают")
+        else:
+            password_hash = generate_password_hash(password)
+            new_user = User(login=login, password=password_hash)
+            db.session.add(new_user)
+            db.session.commit()
+
+            return redirect(url_for('login_page'))
+
+    return render_template('registry.html')
+
+
+@app.after_request
+def redirect_to_signin(response):
+    if response.status_code == 401:
+        return redirect(url_for('login_page') + '?next=' + request.url)
+    return response
+
 
 if __name__ == "__main__":
     app.run(debug=True)
